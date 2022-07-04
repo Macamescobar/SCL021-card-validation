daca332ba3bf8da94ba4363b01f15a29fc54824f
Macarena M <macarenam@MacBook-Air-de-Macarena.local>
2022-07-04 09:48:10 -0400

    cambios en general

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-import validator from './validator.js';
+//import validator from './validator.js';
 
-console.log(validator); 
+//console.log(validator); 
+
+
+let tarjeta = document.querySelector('#tarjeta');
+let formulario = document.querySelector('#form-tarjeta');
+let inputFormulario = document.querySelector('#numeroTarjeta');
+let numeroTarjeta = document.querySelector('#inputNumero');
+let button = document.querySelector('#btnEnviar');
+
+
+button.addEventListener("click", function (e){
+    alert("clickkkk");
+});
+
+numeroTarjeta.value = inputFormulario;
+
+
+
+console.log(tarjeta);
+console.log(formulario);
+console.log(numeroTarjeta);
+console.log(inputFormulario);
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,8 @@
 const validator = {
-  // ...
+  //TODO: develop the function to validate.
+  // git add . => agrega los archivos modificados
+  // git commit -m "" => agrega el mensaje de lo que hiciste 
+  // git push => sube los cambios
 };
 
 export default validator;
