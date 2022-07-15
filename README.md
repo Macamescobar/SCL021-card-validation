169adfb94e57ebc5a1046554685d652d1faf4ab6
Macarena M <macarenam@MacBook-Air-de-Macarena.local>
2022-07-15 12:25:13 -0400

    readme

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Tarjeta de crédito válida
 
-Este proyecto esta pensado para ingresar un número de tarjeta de crédito, de esta forma, se podrá determinar si su tarjeta es válida o no. Es decir si es un documento vigente.
-
+Este proyecto esta pensado para cualquier usuario que posea una tarjeta de crédito, el cual podrá ingresar su número de tarjeta y verificar si es válida o no. 
 El prototipo en un inicio fue el siguiente:
 
 ![Prototipo](/src/img/Prototipo.png)
--- src/index.js
+++ src/index.js
@@ -1,51 +1,43 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
+let formulario = document.querySelector("#form-tarjeta");
 
-let formulario = document.querySelector('#form-tarjeta');
+let numFormulario = document.querySelector("#inputNumero"); //Selecciono el input del form
+let numTarjeta = document.querySelector("#numeroTarjeta"); //Selecciono el p de la tarjeta
+let nameFormulario = document.querySelector("#inputNombre"); //Selecciono el nombre del form
+let nameTarjeta = document.querySelector("#name"); //Selecciono el nombre de la tarjeta
+let button = document.querySelector("#btnEnviar");
 
-
-let numFormulario = document.querySelector('#inputNumero'); //Selecciono el input del form
-let numTarjeta = document.querySelector('#numeroTarjeta'); //Selecciono el p de la tarjeta
-let nameFormulario = document.querySelector('#inputNombre'); //Selecciono el nombre del form
-let nameTarjeta = document.querySelector('#name'); //Selecciono el nombre de la tarjeta
-let button = document.querySelector('#btnEnviar');
-
-
-button.addEventListener("click", function (){
-    validator.isValid(numFormulario.value)
-    if (validator.isValid(numFormulario.value) === true) {
-        alert ('Su tarjeta es valida')
-    } else {
-        alert('Su tarjeta no es valida')
-    }
-    numFormulario.value = validator.maskify(numFormulario.value)
+button.addEventListener("click", function () {
+  if (numFormulario.value == "" && numFormulario.type == "text") {
+    alert("Campos vacios");
+    return false;
+  }
+  validator.isValid(numFormulario.value);
+  if (validator.isValid(numFormulario.value) === true) {
+    alert("Su tarjeta es valida");
+  } else alert("Su tarjeta no es valida");
 });
+numFormulario.value = validator.maskify(numFormulario.value);
 
 //Reflejar los numeros del input en la tarjeta
-numFormulario.addEventListener("input", function(evento) {
-    numTarjeta.innerHTML = evento.target.value
-    numTarjeta.innerHTML = validator.maskify(numTarjeta.innerHTML) //pasar el valor de maskify a la tarjeta
+numFormulario.addEventListener("input", function (evento) {
+  numTarjeta.innerHTML = evento.target.value;
+  numTarjeta.innerHTML = validator.maskify(numTarjeta.innerHTML); //pasar el valor de maskify a la tarjeta
 });
 
 //Reflejar el nombre del input en la tarjeta
 nameFormulario.addEventListener("input", function (evento) {
-    nameTarjeta.innerHTML = evento.target.value
-    
+  nameTarjeta.innerHTML = evento.target.value;
 });
 
-
-formulario.inputNumero.addEventListener('keyup',(e) => {
-    let valorInput = e.target.value; //acedemos al elemento y su valor, y lo guardo en la variable valorInput
-    
-    formulario.inputNumero.value = valorInput
+formulario.inputNumero.addEventListener("keyup", (e) => {
+  let valorInput = e.target.value; //acedemos al elemento y su valor, y lo guardo en la variable valorInput
+  formulario.inputNumero.value = valorInput
     //Eliminar espacios en blanco
-    .replace(/\s/g, '')
-    //Eliminar las letras
-    .replace(/\D/g, '')
+    .replace(/\s/g, "")
+    //Eliminar los numeros
+    .replace(/\D/g, "")
     //Eliminar último espacio
-    .trim() 
+    .trim();
 });
-
-
-
-
